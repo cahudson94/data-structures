78091aaf4de49e0a116405c4407457ae701a25bd
Morgan Nomura <morganelle@gmail.com>
2017-05-22 20:35:51 -0700

    added test for push method

--- src/test_linked_list.py
+++ src/test_linked_list.py
@@ -1,12 +1,15 @@
 """Tests for linked list implementation."""
 from linked_list import Linked_List, Node
 
+EMPTY_LIST = Linked_List()
+
 
 def test_linked_list_push():
     """Test linked list push method."""
-    assert Linked_List.push == Linked_List.push
+    EMPTY_LIST.push(5)
+    assert EMPTY_LIST.head.get_val() == 5
 
 
-def test_linked_list_search():
-    """Test linked list search method."""
-    assert Linked_List.search == Linked_List.search
+# def test_linked_list_search():
+#     """Test linked list search method."""
+#     assert Linked_List.search == Linked_List.search

4dea8bf090296cefd36165218c7046c7e40344ac
Chris Hudson <c.ahudson84@yahoo.com>
2017-05-23 13:08:45 -0700

    update

--- src/linked_list.py
+++ src/linked_list.py
@@ -4,26 +4,36 @@
 class Linked_List(object):
     """."""
 
-    def __init__(self, head=None):
+    def __init__(self, inbound_data=None):
         """."""
-        self.head = head
+        self.head = None
+        self._length = 0
+        if type(inbound_data) in [list, tuple, str]:
+            for item in inbound_data:
+                self.push(item)
+        elif inbound_data is not None:
+            raise TypeError('Give us an interable please.')
 
     def push(self, val):
         """Create a new node."""
-        new_node = Node(val)
-        new_node.set_next(self.head)
+        if not val:
+            raise ValueError('You must give a value.')
+        new_node = Node(val, self.head)
         self.head = new_node
+        self._length += 1
 
-    def remove(self, node):  # infinite loop!
-        """Remove a node from the linked list."""
-        # next_node = node.get_next()
-        # previous_node = node.get_next()
-        # while previous_node != node:
-        #     print(previous_node)
-        #     if previous_node == node:
-        #         break
-        #     previous_node = node.get_next()
-        # previous_node.set_next(next_node)
+    def pop(self):
+        """."""
+        current_node = self.head
+        if current_node is None:
+            raise IndexError('Linked list is empty, no node to pop.')
+        print(current_node)
+        self._length -= 1
+        self.head = current_node.get_next()
+
+    def size(self):
+        """Return the size of a linked list."""
+        return self._length
 
     def search(self, val):
         """Return a node for a given value."""
@@ -34,29 +44,46 @@ class Linked_List(object):
             current_node = current_node.get_next()
         return None
 
-    def size(self):
-        """Return the size of a linked list."""
-        count = 0
+    def remove(self, node):
+        """Remove a node from the linked list."""
+        current_node = self.head
+        previous_node = None
+        while current_node != node:
+            previous_node = current_node
+            current_node = current_node.get_next()
+            if current_node is None:
+                raise ValueError('Node not in linked list.')
+        if previous_node is None:
+            self.head = current_node.get_next()
+        else:
+            previous_node.set_next(current_node.get_next())
+        current_node.set_next(None)
+
+    def display(self):
+        """Return unicde string.
+
+        Represent the list like it is a python tuple.
+        """
+        linked_list = []
         current_node = self.head
         while current_node:
-            count += 1
+            linked_list.append(current_node.val)
             current_node = current_node.get_next()
-        return count
+        return linked_list
 
     def __len__(self):
         """Return the size of a linked list, overwriting len method."""
-        count = 0
-        current_node = self.head
-        while current_node:
-            count += 1
-            current_node = current_node.get_next()
-        return count
+        return self._length
+
+    def __repr__(self):
+        """Print the what is returned by display."""
+        return self.display()
 
 
-class Node(object):
+class Node(object):  # pragma: no cover
     """."""
 
-    def __init__(self, val=None, next_node=None):
+    def __init__(self, val, next_node=None):
         """."""
         self.val = val
         self.next_node = next_node
--- src/test_linked_list.py
+++ src/test_linked_list.py
@@ -2,21 +2,38 @@
 from linked_list import Linked_List, Node
 
 EMPTY_LIST = Linked_List()
+POP_LIST = Linked_List()
+POP_LIST.push('one')
+POP_LIST.push('two')
+POP_LIST.push('three')
 LIST_OF_THREE = Linked_List()
 LIST_OF_THREE.push('one')
 LIST_OF_THREE.push('two')
 LIST_OF_THREE.push('three')
+REMOVE_LIST = Linked_List()
+REMOVE_LIST.push('orange')
+REMOVE_LIST.push('apple')
+REMOVE_LIST.push('grape')
 
 
 def test_linked_list_push():
     """Test linked list push method."""
     EMPTY_LIST.push(5)
-    assert EMPTY_LIST.head.get_val() == 5
+    assert EMPTY_LIST.head.val == 5
 
 
-def test_linked_list_len():
-    """Test for len method."""
-    assert len(LIST_OF_THREE) == 3
+def test_linked_list_push_not_val():
+    """."""
+
+
+def test_linked_list_push_mult():
+    """."""
+
+
+def test_linked_list_pop():
+    """Test linked list pop method."""
+    POP_LIST.pop()
+    assert POP_LIST.head.val == 'two'
 
 
 def test_linked_list_size():
@@ -27,3 +44,22 @@ def test_linked_list_size():
 def test_linked_list_search():
     """Test linked list search method."""
     assert LIST_OF_THREE.search('two').get_val() == 'two'
+
+'''
+def test_linked_list_remove():
+    """Test linked list remove method."""
+    REMOVE_LIST.__print__()
+    REMOVE_LIST.remove('apple')
+    assert REMOVE_LIST.size() == 2
+'''
+def test_linked_list_display():
+    """Test linked list display method."""
+
+
+def test_linked_list_len():
+    """Test for len method."""
+    assert len(LIST_OF_THREE) == 3
+
+
+def test_linked_list_print():
+    """Test linked list print method."""

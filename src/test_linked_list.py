39135e64b45180a647790ff57d367e3a8ba9151c
Morgan Nomura <morganelle@gmail.com>
2017-05-22 20:27:58 -0700

    initial imports for test module

---
+++ src/test_linked_list.py
@@ -0,0 +1,12 @@
+"""Tests for linked list implementation."""
+from linked_list import Linked_List, Node
+
+
+def test_linked_list_push():
+    """Test linked list push method."""
+    assert Linked_List.push == Linked_List.push
+
+
+def test_linked_list_search():
+    """Test linked list search method."""
+    assert Linked_List.search == Linked_List.search

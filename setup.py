f701c2827f61fb7323a3e9d07583461a5534cb6f
Chris Hudson <c.ahudson84@yahoo.com>
2017-05-22 17:14:01 -0700

    setup.py and tox.ini

---
+++ setup.py
@@ -0,0 +1,24 @@
+"""Setup for mail.py."""
+from setuptools import setup
+
+extra_packages = {
+    'testing': ['ipython', 'pytest', 'pytest-watch', 'pytest-cov', 'tox']
+}
+
+setup(
+    name='Data structures',
+    desctription='Implements various data structures.',
+    version='0.1',
+    author='Chris Hudson, Morgan Nomura',
+    author_email='c.ahudson84@yahoo.com, morganelle@gmail.com',
+    license='MIT',
+    py_modules=['linked_list'],
+    package_dir={'': 'src'},
+    install_requires=[],
+    extras_require=extra_packages,
+    entry_points={
+        'console_scripts': [
+            'linked_list = linked_list:main'
+        ]
+    }
+)

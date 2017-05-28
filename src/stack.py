df21aede0d53070522422550a7d72944be09404a
Chris Hudson <c.ahudson84@yahoo.com>
2017-05-28 00:11:25 -0700

    updating ll and stack files

--- src/stack.py
+++ src/stack.py
@@ -7,15 +7,17 @@ class Stack(object):
 
     def __init__(self, inbound_data=None):
         """Create new Stack composing from LinkedList."""
-        self.inbound_data = None
         self._linked_list = LinkedList(inbound_data)
+        self.head = self._linked_list.head
 
     def push(self, val):
         """Return a new node on the Stack using LinkedList.push."""
         self._linked_list.push(val)
+        self.head = self._linked_list.head
 
     def pop(self):
         """Return pop method for LinkedList on Stack."""
+        self.head = self._linked_list.head
         return self._linked_list.pop()
 
     def __len__(self):
--- src/test_stack.py
+++ src/test_stack.py
@@ -1,66 +1,104 @@
 """Tests for stack class implementation."""
-
 from stack import Stack
 import pytest
 
-EMPTY_LIST = Stack()
+EMPTY_STACK_FOR_PUSH = Stack()
+EMPTY_STACK_FOR_POP = Stack()
+
 POP_LIST = Stack()
 POP_LIST.push('one')
-POP_LIST.push('two')
+POP_LIST.push(2)
 POP_LIST.push('three')
+
 LIST_OF_THREE = Stack()
-LIST_OF_THREE.push('one')
-LIST_OF_THREE.push('two')
-LIST_OF_THREE.push('three')
-REMOVE_LIST = Stack()
-REMOVE_LIST.push('orange')
-REMOVE_LIST.push('apple')
-REMOVE_LIST.push('grape')
-LIST_LIST = Stack([1, 2, 3])
+LIST_OF_THREE.push(3)
+LIST_OF_THREE.push(['one', 'two', 'three'])
+LIST_OF_THREE.push((1, 2, 3))
+
+LIST_LIST = Stack([3, 2, 1])
+
 TUPLE_LIST = Stack((2, 4, 9))
+
 STRING_LIST = Stack('cake')
 
-
-def test_stack_pop_empty():
-    """Test IndexError when pop method called on empty stack."""
-    with pytest.raises(IndexError):
-        EMPTY_LIST.pop()
+LEN_OF_THREE = Stack('cake')
+LEN_OF_THREE.push('pie')
+LEN_OF_THREE.push(42)
 
 
-def test_stack_push_none():
-    """Test ValueError when push method called with arg of None."""
-    with pytest.raises(ValueError):
-        EMPTY_LIST.push(None)
+def test_stack_init_list():
+    """Test length and head of Stack when instantiated with a list."""
+    assert len(LIST_LIST) == 3
+    assert LIST_LIST._linked_list.head.val == 1
 
 
-def test_stack_push():
-    """Test Stack push method with valid arg."""
-    EMPTY_LIST.push(5)
-    assert EMPTY_LIST._linked_list.head.val == 5
+def test_stack_init_tuple():
+    """Test length and head of Stack when instantiated with a tuple."""
+    assert len(TUPLE_LIST) == 3
+    assert TUPLE_LIST._linked_list.head.val == 9
 
 
-def test_stack_stack_not_iter():
+def test_stack_init_string():
+    """Test length and head of Stack when instantiated with a string."""
+    assert len(STRING_LIST) == 4
+    assert STRING_LIST._linked_list.head.val == 'e'
+
+
+def test_stack_init_not_iter():
     """Test IndexError when Stack instantiated with non-iterable."""
     with pytest.raises(TypeError):
         Stack(3)
 
 
-def test_stack_push_mult():
-    """Test confirm length of Stack when instantiated with an iterable arg."""
-    assert len(LIST_LIST) == 3
-    assert LIST_LIST._linked_list.head.val == 3
-    assert len(TUPLE_LIST) == 3
-    assert TUPLE_LIST._linked_list.head.val == 9
-    assert len(STRING_LIST) == 4
-    assert STRING_LIST._linked_list.head.val == 'e'
+def test_stack_push_none():
+    """Test ValueError when push method called with arg of None."""
+    with pytest.raises(ValueError):
+        EMPTY_STACK_FOR_PUSH.push(None)
 
 
-def test_stack_pop():
-    """Test linked list pop method."""
-    POP_LIST.pop()
-    assert POP_LIST._linked_list.head.val == 'two'
+def test_stack_push_one():
+    """Test Stack push method with valid arg."""
+    EMPTY_STACK_FOR_PUSH.push(5)
+    assert EMPTY_STACK_FOR_PUSH._linked_list.head.val == 5
+
+
+def test_stack_push_two():
+    """Test Stack push method with valid arg."""
+    EMPTY_STACK_FOR_PUSH.push(7)
+    assert EMPTY_STACK_FOR_PUSH._linked_list.head.val == 7
+
+
+def test_stack_pop_one():
+    """Test stack pop method for string."""
+    assert POP_LIST.pop() == 'three'
+    assert POP_LIST._linked_list.head.val == 2
+
+
+def test_stack_pop_two():
+    """Test stack pop method for int."""
+    assert POP_LIST.pop() == 2
+    assert POP_LIST._linked_list.head.val == 'one'
+
+
+def test_stack_pop_tup():
+    """Test stack pop method for tup as node val."""
+    LIST_OF_THREE.pop()
+    assert LIST_OF_THREE._linked_list.head.val == ['one', 'two', 'three']
+
+
+def test_stack_pop_list():
+    """Test stack pop method for list as node val."""
+    LIST_OF_THREE.pop()
+    assert LIST_OF_THREE._linked_list.head.val == 3
+
+
+def test_stack_pop_empty():
+    """Test IndexError when pop method called on empty stack."""
+    with pytest.raises(IndexError):
+        EMPTY_STACK_FOR_POP.pop()
 
 
 def test_stack_len():
     """Test for len method."""
-    assert len(LIST_OF_THREE) == 3
+    assert len(LEN_OF_THREE) == 6
+    assert len(STRING_LIST) == 4

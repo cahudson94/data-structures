b64b6f467107aa3d95b93eeee7ac36ca7670717e
Morgan Nomura <morganelle@gmail.com>
2017-06-03 21:32:45 -0700

    added more passing tests for nodes and edges

--- src/test_graph_1.py
+++ src/test_graph_1.py
@@ -42,7 +42,6 @@ def three_node_with_two_edges_graph():
     g = Graph()
     g.add_node('corn')
     g.add_node(2)
-    import pdb; pdb.set_trace()
     g.add_node(['squash', 11])
     g.add_edge('corn', 2)
     g.add_edge(['squash', 11], 'corn')
@@ -92,3 +91,54 @@ def test_nodes_in_five_node_graph(five_node_with_five_edges_graph):
                                                        11.1], {'pie': 'cherry',
                                                        'cake': 'chocolate'},
                                                        (4, 'mustard', 'ketchup')]
+
+
+def test_has_node_one_node(one_node_graph):
+    """Test one-node graph has_node method."""
+    assert one_node_graph.has_node('corn') is True
+    assert one_node_graph.has_node([1, 2, 3]) is False
+
+
+def test_has_node_two_nodes(two_node_no_edge_graph):
+    """Test two-node graph has_node method."""
+    assert two_node_no_edge_graph.has_node('beans') is True
+    assert two_node_no_edge_graph.has_node('picante') is False
+
+
+def test_has_node_five_nodes(five_node_with_five_edges_graph):
+    """Test five-node graph has_node method."""
+    assert five_node_with_five_edges_graph.has_node('picante') is False
+    assert five_node_with_five_edges_graph.has_node(11.1) is False
+    assert five_node_with_five_edges_graph.has_node(2) is True
+    assert five_node_with_five_edges_graph.has_node({'pie': 'cherry', 'cake': 'chocolate'}) is True
+    assert five_node_with_five_edges_graph.has_node(['squash', 11.1]) is True
+
+
+def test_edges_empty_graph(empty_graph):
+    """Test an empty graph for edges."""
+    assert empty_graph.edges() == []
+
+
+def test_edges_one_node_graph(one_node_graph):
+    """Test an empty graph for edges."""
+    assert one_node_graph.edges() == []
+
+
+def test_edges_two_node_no_edge_graph(two_node_no_edge_graph):
+    """Test an empty graph for edges."""
+    assert two_node_no_edge_graph.edges() == []
+
+
+def test_edges_two_node_edge(two_node_with_edge_graph):
+    """Test an empty graph for edges."""
+    assert two_node_with_edge_graph.edges() == [('corn', 'beans')]
+
+
+def test_edges_three_node_edge(three_node_with_two_edges_graph):
+    """Test an empty graph for edges."""
+    assert three_node_with_two_edges_graph.edges() == [('corn', 2),(['squash', 11], 'corn')]
+
+
+def test_edges_five_node_edge(five_node_with_five_edges_graph):
+    """Test an empty graph for edges."""
+    assert five_node_with_five_edges_graph.edges() == [(2, ['squash', 11.1]), ((4, 'mustard', 'ketchup'), 'corn'), (2, 'corn'), ('corn', 2), (['squash', 11.1], 'corn')]

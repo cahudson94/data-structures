6500a1f657d2de603a8fd81e7a712f1b4ba65c89
Chris Hudson <c.ahudson84@yahoo.com>
2017-06-20 12:20:06 -0700

    added travis adn fixed some stuff.

--- src/bst.py
+++ src/bst.py
@@ -6,13 +6,16 @@ class BST():
 
     def __init__(self, iterable=None):
         """."""
-        self.head = None
+        self._root = None
         self._length = 0
         self._depth = 0
         self._balance = 0
         if type(iterable) in [tuple, list]:
             for i in iterable:
-                self.insert(i)
+                if type(i) == int:
+                    self.insert(i)
+                raise TypeError('''
+Try again with only numbers in your list or tuple.''')
         elif type(iterable) == int:
             self.insert(iterable)
         elif iterable is not None:
@@ -20,7 +23,9 @@ class BST():
 
     def insert(self, val):
         """."""
-        curr = self.head
+        if type(val) != int:
+            raise TypeError('You can only add numbers to this tree.')
+        curr = self._root
         path = []
         iteration = 0
         left = False
@@ -28,7 +33,7 @@ class BST():
         bal_chg = False
         if curr is None:
             curr = Node(val)
-            self.head = curr
+            self._root = curr
             self._length += 1
             path.append(curr)
             if len(path) > self._depth:
@@ -37,53 +42,57 @@ class BST():
         path.append(curr)
         while True:
             if val == curr.val:
-                return None
-            elif val < curr.val and curr.left is None:
-                curr.left = Node(val)
-                self._length += 1
-                path.append(curr.left)
-                if len(path) > self._depth:
-                    self._depth = len(path)
-                    bal_chg = True
-                if iteration == 0:
-                    left = True
-                    iteration += 1
-                if left and bal_chg:
-                    self._balance += 1
-                if right and bal_chg:
-                    self._balance -= 1
                 return
-            elif val > curr.val and curr.right is None:
-                curr.right = Node(val)
-                self._length += 1
-                path.append(curr.right)
-                if len(path) > self._depth:
-                    self._depth = len(path)
-                    bal_chg = True
-                if iteration == 0:
-                    right = True
-                    iteration += 1
-                if left and bal_chg:
-                    self._balance += 1
-                if right and bal_chg:
-                    self._balance -= 1
-                return
-            elif val < curr.val and curr.left:
-                path.append(curr)
-                curr = curr.left
-                if iteration == 0:
-                    left = True
-                    iteration += 1
-            elif val > curr.val and curr.right:
-                path.append(curr)
-                curr = curr.right
-                if iteration == 0:
-                    right = True
-                    iteration += 1
+            elif val < curr.val:
+                if curr.left:
+                    path.append(curr)
+                    curr = curr.left
+                    if iteration == 0:
+                        left = True
+                        iteration += 1
+                else:
+                    curr.left = Node(val)
+                    self._length += 1
+                    path.append(curr.left)
+                    if len(path) > self._depth:
+                        self._depth = len(path)
+                        bal_chg = True
+                    if iteration == 0:
+                        left = True
+                        iteration += 1
+                    if left and bal_chg:
+                        self._balance += 1
+                    if right and bal_chg:
+                        self._balance -= 1
+                    return
+            elif val > curr.val:
+                if curr.right:
+                    path.append(curr)
+                    curr = curr.right
+                    if iteration == 0:
+                        right = True
+                        iteration += 1
+                else:
+                    curr.right = Node(val)
+                    self._length += 1
+                    path.append(curr.right)
+                    if len(path) > self._depth:
+                        self._depth = len(path)
+                        bal_chg = True
+                    if iteration == 0:
+                        right = True
+                        iteration += 1
+                    if left and bal_chg:
+                        self._balance += 1
+                    if right and bal_chg:
+                        self._balance -= 1
+                    return
 
     def search(self, val):
         """."""
-        curr = self.head
+        curr = self._root
+        if type(val) != int:
+            raise TypeError('This tree only contains numbers.')
         while True:
             if val == curr.val:
                 return curr
@@ -104,6 +113,8 @@ class BST():
 
     def contains(self, val):
         """."""
+        if type(val) != int:
+            raise TypeError('This tree only contains numbers.')
         if self.search(val):
             return True
         return False
@@ -116,8 +127,8 @@ class BST():
 class Node():
     """."""
 
-    def __init__(self, val):
+    def __init__(self, val, left=None, right=None):
         """."""
         self.val = val
-        self.left = None
-        self.right = None
+        self.left = left
+        self.right = right

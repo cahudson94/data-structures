384a1e38741e7b488e448b3036d87df83573d911
Chris Hudson <c.ahudson84@yahoo.com>
2017-07-15 13:41:55 -0700

    fixed up testing.

--- src/working/merge_sort.py
+++ src/working/merge_sort.py
@@ -2,9 +2,10 @@
 
 
 def mergesort(data):
+    """Sorting algorithm for merg sort."""
     try:
         if len(data) > 1:
-            half = len(data)//2
+            half = len(data) // 2
             left = data[:half]
             right = data[half:]
 
@@ -34,42 +35,37 @@ def mergesort(data):
                 ri += 1
                 di += 1
 
-        # return data
+        return data
 
     except TypeError:
         return 'This sort method only accepts values of one type in a list.'
 
 
-data = [33, 5, 66, 77, 43, 343, 3434, 53, 2, 1, 45, 6]
-mergesort(data)
-print(data)
-
-
-# if __name__ == '__main__':  # pragma: no cover
-#     from timeit import Timer
-#     best = Timer(
-#         'mergesort([x for x in range(100)])',
-#         "from __main__ import mergesort"
-#     )
-#     worst = Timer(
-#         'mergesort([x for x in range(100)][::-1])',
-#         "from __main__ import mergesort; from random import randint"
-#     )
-#     random = Timer(
-#         'mergesort([randint(0, 1000) for x in range(100)][::-1])',
-#         "from __main__ import mergesort; from random import randint"
-#     )
-#     print("""
-# Merge sort is a simple sorting algorithm that repeatedly steps through
-# the list to be sorted, compares each pair of adjacent items and swaps
-# them if they are in the wrong order.
-# """)
-#     print("#================= best case search 10000x ==============#")
-#     print(best.timeit(number=1000))
-#     print('')
-#     print("#================= worse case search 10000x==============#")
-#     print(worst.timeit(number=1000))
-#     print('')
-#     print("#================= random case search 10000x=============#")
-#     print(worst.timeit(number=1000))
-#     print('')
+if __name__ == '__main__':  # pragma: no cover
+    from timeit import Timer
+    best = Timer(
+        'mergesort([x for x in range(100)])',
+        "from __main__ import mergesort"
+    )
+    worst = Timer(
+        'mergesort([x for x in range(100)][::-1])',
+        "from __main__ import mergesort; from random import randint"
+    )
+    random = Timer(
+        'mergesort([randint(0, 1000) for x in range(100)][::-1])',
+        "from __main__ import mergesort; from random import randint"
+    )
+    print("""
+Merge sort is a simple sorting algorithm that repeatedly steps through
+the list to be sorted, compares each pair of adjacent items and swaps
+them if they are in the wrong order.
+""")
+    print("#================= best case search 10000x ==============#")
+    print(best.timeit(number=1000))
+    print('')
+    print("#================= worse case search 10000x==============#")
+    print(worst.timeit(number=1000))
+    print('')
+    print("#================= random case search 10000x=============#")
+    print(worst.timeit(number=1000))
+    print('')

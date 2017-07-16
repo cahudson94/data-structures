a82cbf68773f4908401b65e57c26d5579e089511
Chris Hudson <c.ahudson84@yahoo.com>
2017-07-15 18:42:08 -0700

    updating files part 1.

--- src/working/test_bubble_sort.py
+++ src/working/test_bubble_sort.py
@@ -7,20 +7,19 @@ PARAMS_TABLE = [
     ([4, 3, 2, 1], [1, 2, 3, 4]),
     ((99, 22, 55, 4, 66, 87, 23, 11), [4, 11, 22, 23, 55, 66, 87, 99]),
     ([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]),
-    ('sfajlfka', ['a', 'a', 'f', 'f', 'j', 'k', 'l', 's'])
 ]
 
 
 def test_strings_error_handling_in_bubble_sort():
     """Test that an error is thrown if there is a string."""
-    assert bubble_sort([1, 'blkja', 2324, 3]) == 'Please only pass in an \
-iterable of numbers.'
+    with pytest.raises(TypeError):
+        bubble_sort([1, 'blkja', 2324, 3])
 
 
 def test_iterable_type_error_handling():
     """Test if not list or tuple, ValueError raised."""
-    assert bubble_sort(('sfajlfka', 1, 5)) == 'Please only pass in an \
-iterable of numbers.'
+    with pytest.raises(TypeError):
+        bubble_sort(('sfajlfka', 1, 5))
 
 
 @pytest.mark.parametrize('data, result', PARAMS_TABLE)
